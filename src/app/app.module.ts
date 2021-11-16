d1648b5a7ac4d52004e3c12c34fecac257387c1a
soManyAccounts <matthewdcairns@gmail.com>
2021-11-16 11:32:14 -0500

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavBar } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, NavBar],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
