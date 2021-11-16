80549f447186b8314e63747d3706ff25e65d1a39
soManyAccounts <matthewdcairns@gmail.com>
2021-11-16 11:52:37 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavBar } from './nav-bar/nav-bar.component';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar/toolbar-module';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, MatToolbarModule],
