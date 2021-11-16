81e46161b021adcf8aeb37bcdb64b12aa6b99579
soManyAccounts <matthewdcairns@gmail.com>
2021-11-16 12:00:26 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavBar } from './nav-bar/nav-bar.component';
 import { MatToolbarModule } from '@angular/material/toolbar/toolbar-module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatToolbarModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+  ],
   declarations: [AppComponent, HelloComponent, NavBar],
   bootstrap: [AppComponent],
 })
